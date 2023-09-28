a625529685520f4a3d526892817a80994cbd20d9
Oladejo Toyyib <oladejotoyyibolalekan@gmail.com>
2023-09-28 08:50:24 +0100

    testing the api file

--- app/components/TodoList.tsx
+++ app/components/TodoList.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-const TodoList = () => {
+interface TodoListProps {
+    tasks: ITask[];
+}
+
+const TodoList: React.FC<TodoListProps> = ({ tasks }) => {
   return (
     <div className="overflow-x-auto">
         <table className="table">
@@ -12,11 +16,12 @@ const TodoList = () => {
             </tr>
             </thead>
             <tbody>
-            {/* row 1 */}
-            <tr> 
-                <td>Cy Ganderton</td>      
-                <td>Blue</td>
-            </tr>
+                {tasks.map((task) => (
+                <tr key={task.id}> 
+                    <td>{task.text}</td>      
+                    <td>Blue</td>
+                </tr>   
+                ))}
             </tbody>
         </table>
 </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import AddTask from "./components/AddTask";
 import TodoList from "./components/TodoList";
+import { getAllTodos } from "@/api";
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await getAllTodos()
+  console.log(tasks)
   return (
     <main className="mx-auto max-w-4xl mt-4 ">
       <section className="my-5 flex flex-col gap-4 text-center"  >
